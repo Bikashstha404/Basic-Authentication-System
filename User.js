f5e89976209c88f65951c777ebb11f726d368fac
Bikash Shrestha <stha.bikam99@gmail.com>
2024-04-08 16:07:36 +0545

    Admin and Moderator Route

--- User.js
+++ User.js
@@ -10,12 +10,26 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        lowercase: true
+        lowercase: true,
+        validate: {
+            validator: function(value) {
+                // Regular expression for email validation
+                const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                return emailRegex.test(value);
+              },
+              message: 'Email must be a valid email address'
+        }
     },
     password: {
         type: String,
         required: true,
         minlength: 5,
+    }, 
+    role: {
+        type: String,
+        default: 'user',
+        lowercase: true,
+        required: true
     }
 })
 
--- server.js
+++ server.js
@@ -29,23 +29,38 @@ app.get('/register', (req, res)=>{
 })
 app.post('/register',async(req,res)=>{
 
-    const{ name, email, password} = req.body
+    const{ name, email, password, role} = req.body
 
     try{
+        if(!email){
+            res.json({error: "Email is not provided."})
+            return;
+        }
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email format." });
+        }
         
         const existingUser = await User.findOne({ email })
         if(existingUser){
             return res.status(400).json({ error: "Email already exists"})
         }
 
+        if(!name || !password){
+            res.json({error: "Either Name or Password is not provided."})
+            return;
+        }
+        
         bcrypt.hash(password, 10, async(error, hashPassword)=>{
             if(error){
                 return res.status(500).json({ error: `Error hashing password: ${error}` });
             }
-            const newUser = new User({ 
+            const newUser = new User({
                 name: name, 
                 email: email, 
-                password: hashPassword })
+                password: hashPassword,
+                role: role    
+            })
                 
             await newUser.save()
             res.send("Registration Successful")
@@ -53,13 +68,13 @@ app.post('/register',async(req,res)=>{
     }
     catch(e){
         console.log(e.message)
-        return res.status(500).json({ error: "Server Error"})
+        return res.json({ error: "Server Error"})
     }
     
 })
 
 app.get('/login', (req, res)=>{
-    res.render('login')
+    res.render("login")
 })
 
 app.post('/login',async(req, res)=>{
@@ -68,20 +83,28 @@ app.post('/login',async(req, res)=>{
     try{
 
         const user = await User.findOne({ email})
-  
         if (!user || !bcrypt.compareSync(password, user.password)) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
 
-        const token = jwt.sign({user}, "secretkey", {expiresIn: "1h"});
-
+        const token = jwt.sign({user, role: user.role}, "secretkey", {expiresIn: "1h"});
         res.cookie('token', token, {
             httpOnly: true
         })
-
-        res.send("Login Successful")
-        // res.redirect(`/profile`)
+        console.log(user.role)
+        if(user.role == "user"){
+            res.send("User profile")
+            // res.redirect("/profile");
         }
+        if(user.role == "admin"){
+            res.send("Admin Page Successful")
+            // res.redirect(`/admin`)
+        }
+        if(user.role == "moderator"){
+            res.send("Login Successful")
+            // res.redirect(`/moderator`)
+        }
+    }
     catch(e){
         console.log(e.message)
         return res.status(500).json("Server Error")
@@ -94,7 +117,7 @@ app.get('/profile',authenticate, (req, res)=>{
 })
 
 app.post('/profile', authenticate, (req,res)=>{
-    res.send(`${req.user.name}`)
+    res.send(`Welcome, ${req.user.name}`)
 })
 
 function authenticate(req,res,next){
@@ -105,6 +128,7 @@ function authenticate(req,res,next){
 
     const verified = jwt.verify(token, "secretkey")
     req.user = verified.user;
+    req.role = verified.role;
     
     next()
 }
@@ -113,6 +137,31 @@ app.get('/logout', (req,res)=>{
     res.clearCookie('token')
     res.redirect('/login')
 })
+
+function isAdmin(req, res, next){
+    res.send(req.role)
+    if(req.user.role != "admin"){
+        return res.status(403).json({error: 'Admin access required.'})
+    }
+    next()
+}
+
+function isModerator(req, res, next){
+    if(req.user.role != "moderator"){
+        return res.status(403).json({error: 'Moderator access required.'})
+    }
+    next()
+}
+
+app.get('/admin', authenticate, isAdmin, (req, res) => {
+    res.json({ message: 'Admin route', user: req.user });
+});
+
+
+app.get('/moderator', authenticate, isModerator, (req, res) => {
+    res.json({ message: 'Moderator route', user: req.user });
+});
+
 
 
 app.listen(4040)
